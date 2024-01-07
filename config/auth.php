cc4712397707348b491a905048f149f6bf93e113
noaama <152269693+noaama@users.noreply.github.com>
2024-01-07 18:38:18 +0100

    modif

--- config/auth.php
+++ config/auth.php
@@ -90,14 +90,8 @@ return [
     |
     */
 
-    'passwords' => [
-        'users' => [
-            'provider' => 'users',
-            'table' => 'password_reset_tokens',
-            'expire' => 60,
-            'throttle' => 60,
-        ],
-    ],
+    
+   
 
     /*
     |--------------------------------------------------------------------------
