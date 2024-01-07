c0218c4df62eb78500604fd862d38f24a13e097b
noaama <152269693+noaama@users.noreply.github.com>
2024-01-07 18:22:54 +0100

    d

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->unique();
             $table->string('token');
-            $table->timestamp('created_at')->nullable();
+            $table->timestamp('created_at');
         });
     }
 
@@ -26,3 +26,4 @@ return new class extends Migration
         Schema::dropIfExists('password_reset_tokens');
     }
 };
+
